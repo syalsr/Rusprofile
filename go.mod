61042991434e5764986114cac90f45a1e4697f50
volobuevrostislav <volobuevrostislav@gmail.com>
2022-10-04 21:08:30 +0300

    Add goquery instead regex

--- go.mod
+++ go.mod
@@ -4,12 +4,9 @@ go 1.18
 
 require (
 	github.com/gin-gonic/gin v1.8.1
-	github.com/go-chi/chi v4.1.2+incompatible
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.11.3
 	github.com/swaggo/files v0.0.0-20220728132757-551d4a08d97a
 	github.com/swaggo/gin-swagger v1.5.3
-	github.com/swaggo/http-swagger v1.3.3
-	github.com/swaggo/http-swagger/example/go-chi v0.0.0-20220809182543-c8d62bfd8fdb
 	github.com/swaggo/swag v1.8.6
 	google.golang.org/genproto v0.0.0-20220921223823-23cae91e6737
 	google.golang.org/grpc v1.49.0
@@ -19,6 +16,8 @@ require (
 
 require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/jsonreference v0.20.0 // indirect
@@ -40,7 +39,7 @@ require (
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
-	golang.org/x/net v0.0.0-20220921203646-d300de134e69 // indirect
+	golang.org/x/net v0.0.0-20221004154528-8021a29435af // indirect
 	golang.org/x/sys v0.0.0-20220919091848-fb04ddd9f9c8 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/tools v0.1.12 // indirect
--- pkg/CompanyByINN.go
+++ pkg/CompanyByINN.go
@@ -3,73 +3,74 @@ package pkg
 import (
 	"Rusprofile/proto"
 	"fmt"
-	"io/ioutil"
+	"github.com/PuerkitoBio/goquery"
 	"net/http"
-	"strings"
+	"sync"
 )
 
 const URL = "https://www.rusprofile.ru/search?query=%v"
 
-func FindKPP(doc string) string {
-	result, err := FindElementByRegex(doc, "clip_kpp\">\\d{9}")
-	if err != nil {
-		return ""
-	}
-	return result[10:]
+func FindKPP(doc *goquery.Document, resp *proto.Response, wg *sync.WaitGroup) {
+	defer wg.Done()
+	doc.Find(".copy_target").Each(
+		func(i int, selection *goquery.Selection) {
+			switch i {
+			case 2:
+				resp.KPP = selection.Text()
+			}
+		},
+	)
 }
 
-func FindDirector(doc string) string {
-	result, err := FindElementByRegex(doc, "person_ul\"><span>(.*)<")
-	if err != nil {
-		return "Company was liquidate"
-	}
-	result = result[17:]
-	result = strings.Replace(result, "<span>", "", -1)
-	result = strings.Replace(result, "</span>", "", -1)
-	result = strings.Replace(result, "</a>", "", -1)
-	result = strings.Replace(result, "<", "", -1)
-	return result
+func FindDirector(doc *goquery.Document, resp *proto.Response, wg *sync.WaitGroup) {
+	defer wg.Done()
+	doc.Find(".gtm_main_fl").Each(
+		func(i int, selection *goquery.Selection) {
+			resp.Director = selection.Text()
+		},
+	)
 }
 
-func FindCompanyName(doc string) string {
-	result, err := FindElementByRegex(doc, "legalName.?>(.*)<")
-	if err != nil {
-		return ""
-	}
-	result = result[11:]
-	result = strings.Replace(result, "&quot;", "", -1)
-	result = strings.Replace(result, "<", "", -1)
-	return result
+func FindCompanyName(doc *goquery.Document, resp *proto.Response, wg *sync.WaitGroup) {
+	defer wg.Done()
+	doc.Find(".company-name").Each(
+		func(i int, selection *goquery.Selection) {
+			resp.CompanyName = selection.Text()
+		},
+	)
 }
 
-func FindCompanyByINN(INN string) (*proto.Response, error) {
+func getHtmlPage(INN string) (*http.Response, error) {
 	response, err := http.Get(fmt.Sprintf(URL, INN))
 	if err != nil {
 		return nil, err
 	}
-	defer response.Body.Close()
-
 	if response.StatusCode != 200 {
 		return nil, fmt.Errorf(response.Status)
 	}
+	return response, nil
+}
 
-	var body []byte
-	body, err = ioutil.ReadAll(response.Body)
+func FindCompanyByINN(INN string) (*proto.Response, error) {
+	resp, err := getHtmlPage(INN)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
 	if err != nil {
 		return nil, err
 	}
 
-	bodyToParse := string(body)
+	response := &proto.Response{INN: INN}
+	wg := &sync.WaitGroup{}
+	wg.Add(3)
 
-	kpp := FindKPP(bodyToParse)
-	companyName := FindCompanyName(bodyToParse)
-	director := FindDirector(bodyToParse)
+	go FindKPP(doc, response, wg)
+	go FindCompanyName(doc, response, wg)
+	go FindDirector(doc, response, wg)
 
-	return &proto.Response{
-			INN:         INN,
-			KPP:         kpp,
-			CompanyName: companyName,
-			Director:    director,
-		},
-		nil
+	wg.Wait()
+	return response, nil
 }
